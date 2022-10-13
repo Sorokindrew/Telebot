f8b62763ddd04ecdd3d158745771e8b656c68606
Andrey Sorokin <sorokindrew@mail.ru>
2022-10-13 15:13:55 +0300

    Handlers for all commands added

---
+++ lowprice.py
@@ -0,0 +1,5 @@
+import requests
+
+
+def lowprice():
+	return 'Узнать топ самых дешевых отелей в городе'
--- main.py
+++ main.py
@@ -1,15 +1,52 @@
 import telebot
+from lowprice import lowprice
+from bestdeal import bestdeal
+from highprice import highprice
+from history import history
 
-my_bot = telebot.TeleBot('5551354218:AAHKwAoizg50q8_euQeXeg7RsWWnZx1kDiM')
+bot = telebot.TeleBot('5551354218:AAHKwAoizg50q8_euQeXeg7RsWWnZx1kDiM')
 
 
-@my_bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет! Чем могу помочь?')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.from_user.id,
+                     'Для работы с ботом, используйте следующие команды:'
+                     '\n/lowprice\n/highprice\n/bestdeal\n/history')
+
+
+@bot.message_handler(commands=['lowprice'])
+def lowprice_handler(message):
+    bot.send_message(message.chat.id, lowprice())
+
+
+@bot.message_handler(commands=['highprice'])
+def highprice_handler(message):
+    bot.send_message(message.chat.id, highprice())
+
+
+@bot.message_handler(commands=['bestdeal'])
+def bestdeal_handler(message):
+    bot.send_message(message.chat.id, bestdeal())
+
+
+@bot.message_handler(commands=['history'])
+def history_handler(message):
+    bot.send_message(message.chat.id, history())
+
+
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'hello':
-        my_bot.send_message(message.from_user.id, 'Hello. Can I help You?')
-    elif message.text == '/help':
-        my_bot.send_message(message.from_user.id, 'For request You may use following commands: \n/lowprice\n/highprice\n/bestdeal\n/history')
+        bot.send_message(message.from_user.id, 'Hello. Can I help You?')
     else:
-        my_bot.send_message(message.from_user.id, 'I do not understand You. Please write /help for instructions')
+        bot.send_message(message.from_user.id,
+                         'I do not understand You. Please write'
+                         ' /help for instructions')
 
-my_bot.polling(non_stop=True, interval=0)
+
+bot.polling(non_stop=True, interval=0)
